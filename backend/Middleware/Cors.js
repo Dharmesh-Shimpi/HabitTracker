2fbcbf6f3a0761ff063abf7d0aac30c6722ac5fe
Dharmesh Shimpi <dharmesh.shimpi@gmail.com>
2024-07-17 20:43:21 +0530

    cors update

--- backend/Middleware/Cors.js
+++ backend/Middleware/Cors.js
@@ -1,5 +1,5 @@
 export const corsOptions = {
-	origin: 'http://localhost:5173', // Allow requests from this origin
+	origin: 'https://habittracker-g2pu.onrender.com', // Allow requests from this origin
 	methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
 	allowedHeaders: 'Content-Type,Authorization', // Allowed request headers
 	credentials: true, // Allow credentials (cookies, authorization headers, etc.)
