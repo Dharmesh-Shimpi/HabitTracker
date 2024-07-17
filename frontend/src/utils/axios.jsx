224a02fe045c3b6ee8aeaf0adc393284d0418481
Dharmesh Shimpi <dharmesh.shimpi@gmail.com>
2024-07-17 23:02:47 +0530

    frontend axios

--- frontend/src/utils/axios.jsx
+++ frontend/src/utils/axios.jsx
@@ -2,9 +2,7 @@ import axios from 'axios';
 // 'https://habittracker-backend-6psa.onrender.com/api'
 // http://localhost:5000/api
 const api = axios.create({
-	baseURL:
-		'https://habittracker-backend-6psa.onrender.com/api',
-	withCredentials: true,
+	baseURL: 'https://habittracker-backend-6psa.onrender.com/api',
 });
 
 export default api;
